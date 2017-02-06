91197f1202f5b68f71f6719a6cc5d2d2b3ef6e77
Gunnar Braaten <gunnar.braaten@gmail.com>
2017-02-06 01:14:48 +0100

    Introduced a simple example API.

---
+++ app/static/index.js
@@ -0,0 +1,8 @@
+const url = new URL(window.location);
+const message = url.searchParams.get('message');
+fetch(`/echo/v1?message=${message}`)
+.then(response => response.json())
+.then((json) => {
+  const header = document.querySelector('h1');
+  header.textContent = json.message;
+});
