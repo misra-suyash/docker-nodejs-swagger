1782334881e6a32f2993b70d2ea9d98569e2e7e5
Gunnar Braaten <gunnar.braaten@gmail.com>
2017-02-06 01:13:24 +0100

    Better handling of Swagger errors.

--- app/server.js
+++ app/server.js
@@ -7,17 +7,6 @@ const app = express();
 
 app.use(express.static(path.join(__dirname, 'static')));
 
-app.use((err, req, res, next) => {
-  console.log(err);
-  if (err.failedValidation) {
-    console.log(err.results);
-    res.status(400).json({ message: err.message, results: err.results });
-  } else {
-    res.status(500).json({ message: err.message });
-  }
-  next();
-});
-
 swaggerTools.initializeMiddleware(swaggerDoc, (middleware) => {
   app.use(middleware.swaggerMetadata());
   app.use(middleware.swaggerValidator({
@@ -27,6 +16,16 @@ swaggerTools.initializeMiddleware(swaggerDoc, (middleware) => {
     controllers: path.join(__dirname, 'routes/'),
   }));
   app.use(middleware.swaggerUi());
+  app.use((err, req, res, next) => {
+    console.log(err);
+    if (err.failedValidation) {
+      console.log(err.results);
+      res.status(400).json({ message: err.message, results: err.results });
+    } else {
+      res.status(500).json({ message: err.message });
+    }
+    next();
+  });
 
   app.listen(3000, () => {
     console.log('Listening on port 3000!');
